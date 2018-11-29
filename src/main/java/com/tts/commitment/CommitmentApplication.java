7874d2172c09d172cf318caf151759295897d43a
Leslie Alvarado <lesliemalvarado@gmail.com>
2018-11-29 00:25:09 -0500

    Add Files

---
+++ src/main/java/com/tts/commitment/CommitmentApplication.java
@@ -0,0 +1,12 @@
+package com.tts.commitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitmentApplication.class, args);
+	}
+}
